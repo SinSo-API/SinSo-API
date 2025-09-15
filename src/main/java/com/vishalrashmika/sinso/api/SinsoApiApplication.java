30179027a6f58ef8554d337114b8b3f60feeb31e
J.M. Vishal Rashmika Perera <vishal.rashmika.perera@gmail.com>
2025-09-15 11:55:50 +0530

    Spring Init + Git-Crypt Setup

---
+++ src/main/java/com/vishalrashmika/sinso/api/SinsoApiApplication.java
@@ -0,0 +1,13 @@
+package com.vishalrashmika.sinso.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinsoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishalrashmika/sinso/api/SinsoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishalrashmika.sinso.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinsoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
